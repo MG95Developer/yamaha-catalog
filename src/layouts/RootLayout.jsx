052ec2ca96e234d061cba934470f83022fbb1de5
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2023-02-25 12:00:37 +0000

    grid responsiveness setup

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -7,39 +7,43 @@ export default function RootLayout()
 {
   return (
     <>
-      <Container border="4px solid orange" maxWidth="1850px" p="0">
-        <Grid templateColumns="repeat(12, 1fr)" bg="gray.50">
+      <Container maxWidth="1850px" p="0">
+        <Grid templateColumns="repeat(12, 1fr)">
 
           <GridItem
             as="section"
-            bg="brown"
+            border="6px solid brown"
             colSpan={{ base: 12 }}
+            minHeight={{ base: '120px', md: "100px" }}
           >
             section: includes logo to left and social media icons to right
           </GridItem> {/*  */}
 
           <GridItem
             as="nav"
-            bg="purple.400"
-            colSpan={{ base: 12 }}
+            border="6px solid purple"
+            colSpan={{ base: 12, md: 4, lg: 4, xl: 3 }}
+            minHeight={{ base: '120px' }}
           >
             navbar:  includes nav-links to all the 4 pages (home, profile, test drive, contact)
           </GridItem>
 
           <GridItem
             as="main"
-            bg="green.400"
-            colSpan={{ base: 12 }}
+            border="6px solid green"
+            colSpan={{ base: 12, md: 8, lg: 8, xl: 9 }}
+            minHeight={{ base: '100vh' }}
           >
             Page: <Outlet />
           </GridItem>
 
           <GridItem
             as="footer"
-            bg="pink.400"
+            border="6px solid pink"
             colSpan={{ base: 12 }}
+            minHeight={{ base: '80px', md: '150px' }}
           >
-            footer:  footer content.
+            footer will go here
           </GridItem>
 
         </Grid>
