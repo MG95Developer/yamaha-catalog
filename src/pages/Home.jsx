65eda9af7ad2359dca8755d187fde37d93ae180b
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2023-02-25 11:05:54 +0000

    create components folder & individual components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
 
 export default function Home()
 {
   return (
     <>
-      <Box bgGradient="radial(gray.300, yellow.400, pink.200)">
-        <Text fontWeight="bold" color="red.600">Homepage</Text>
-      </Box>
+      <Text>Homepage</Text>
     </>
 
   )
