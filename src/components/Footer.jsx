9973167b8f592bc135faceb3b4e6a38bd7641bb4
mgdeveloper95@gmail.com <mgdeveloper95@gmail.com>
2023-03-02 19:05:31 +0000

    planning the footer UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
+import { Box, Heading } from "@chakra-ui/react";
 
 export default function Footer()
 {
     return (
-        <div>My footer details will go here</div>
+        <>
+            <Box display={{ base: 'block', xl: 'none' }} mb="20px">
+                Footer 1 up to xl (up to 1279px) breakpoint: Stack of top of each other: Width 100%, left aligned.
+                React Accordion: Need Help, Shop Now, Inside Yamaha, Our Brands (when the accordion opens the child links can be shown.)
+            </Box>
+
+            <Box display={{ base: 'none', xl: 'block' }} mt="20px">
+                Footer 2 will kick in when screensize is xl (1280px or more) or bigger: 5 COL layout, left aligned.
+            </Box>
+
+        </>
     )
 }
